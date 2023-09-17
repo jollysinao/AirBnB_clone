739ab2d372744c8864dbb92fa94d0525075c287f
Im-adom <graceamaningkwarteng@gmail.com>
2023-09-16 17:47:03 -0700

    State of user

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This defines the class called State."""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """This represents a state."""
+
+    name = ""
