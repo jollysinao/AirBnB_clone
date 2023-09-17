d45aca9afa52bed9bc4e6a41cfd21ae08840c3ed
jollysinao <jollysinao@gmail.com>
2023-09-17 15:44:37 +0000

    amenity changes

--- models/amenity.py
+++ models/amenity.py
@@ -3,10 +3,10 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represents an amenity.
+    """Represent an amenity.
 
     Attributes:
-        name (str): name of amenity.
+        name (str): name of the amenity.
     """
 
     name = ""
